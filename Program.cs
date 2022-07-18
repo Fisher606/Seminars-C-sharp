16fae3125a22fffe693a65e60c01f58e914761ab
Vadim <fishermanvadik@gmail.com>
2022-07-18 03:17:38 +0300

    Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ else
     if(num == 7)Console.WriteLine("Воскресение");
 }
 
-*/
+
 // Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
 int num1, num2;
 Console.Write("Input First Integer Number: " );
@@ -65,4 +65,21 @@ if(num1>num2)
 else
     Console.WriteLine(num2);
 
+*/
 
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int num1, num2, num3, max = 0;
+Console.Write("Input First Integer Number: " );
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Second Integer Number: " );
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input Third Integer Number: " );
+num3 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+    max = num2;
+    if(num3 > max)
+    max = num3;
+else num1 = max;
+
+ Console.Write(max);   
